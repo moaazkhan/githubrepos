635672059ae337447309e0d6e1b6e249f5fbfb0b
Moaaz Khan <moaazkhan33@gmail.com>
2014-12-23 23:07:33 +0500

    Task 1 finally actually complete 100%

--- app/src/main/java/com/githubrepos/githubrepos/MainActivity.java
+++ app/src/main/java/com/githubrepos/githubrepos/MainActivity.java
@@ -1,11 +1,22 @@
 package com.githubrepos.githubrepos;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.apache.http.Header;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 
@@ -16,32 +27,107 @@ public class MainActivity extends Activity implements OnClickListener{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final String ed1;
+
 
         View button = findViewById(R.id.button);
         button.setOnClickListener(this);
 
-        final EditText edit = (EditText) findViewById(R.id.editText);
-      //  final TextView ans = (TextView) findViewById(R.id.textView1);
-        final String owner;
+
+        findViewById(R.id.marker_progress).setVisibility(View.GONE);
+
 
         }
 
 
 
             public void onClick(View view) {
-
-
+                findViewById(R.id.marker_progress).setVisibility(View.VISIBLE);
+                hideKeyboard();
+                    final TextView ans = (TextView) findViewById(R.id.textView1);
                         final EditText edit = (EditText) findViewById(R.id.editText);
                         String a = edit.getText().toString();
                         String path = "/users/" + a + "/repos";
-                Intent i = new Intent(getApplicationContext(), hidden.class);
-                i.putExtra("new_variable_name",path);
-                startActivity(i);
 
+
+
+
+
+
+
+                APIClient.get(path, null, new JsonHttpResponseHandler() {
+                    // GET doesnt need params
+                    // BUT IT DOES NEED A PROPER PATH
+
+
+
+                    @Override
+                    public void onStart() {
+                        ans.setText("Processing");
+
+                    }
+
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONArray
+                            response) {
+                        // 'JSONArray response' because it will be an array of repos
+                        findViewById(R.id.marker_progress).setVisibility(View.GONE);
+
+                        for (int i = 0; i < response.length(); i++) {
+                            JSONObject obj = null;
+                            try {
+                                obj = response.getJSONObject(i);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                            String name = null;
+                            try {
+                                name = obj.getString("name");
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+
+
+                            mylist.add(name); //this adds an element to the list.
+
+
+
+
+
+                        }
+
+                        Intent i = new Intent(getApplicationContext(), hidden.class);
+                        i.putExtra("new_variable_name",mylist);
+                        startActivity(i);
+                        mylist.clear();
+
+
+                    }
+
+                    @Override
+                    //  public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+                    public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject error) {
+                        findViewById(R.id.marker_progress).setVisibility(View.GONE);
+                        ans.setMovementMethod(new ScrollingMovementMethod());
+                        ans.setText("Error:\n\n" + error.toString());
+
+
+
+
+                    }
+
+                });
 
                 }
-            }
+    private void hideKeyboard() {
+        InputMethodManager inputManager = (InputMethodManager) this.getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        // check if no view has focus:
+        View view = this.getCurrentFocus();
+        if (view != null) {
+            inputManager.hideSoftInputFromWindow(view.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+}
 
 
 
--- app/src/main/java/com/githubrepos/githubrepos/hidden.java
+++ app/src/main/java/com/githubrepos/githubrepos/hidden.java
@@ -5,20 +5,12 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import com.loopj.android.http.JsonHttpResponseHandler;
-
-import org.apache.http.Header;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 
 
 public class hidden extends Activity {
 
-static    ArrayList<String> mylist = new ArrayList<String>();
+
 
     static ArrayAdapter<String> listAdapter ;
     ListView listView ;
@@ -30,111 +22,24 @@ static    ArrayList<String> mylist = new ArrayList<String>();
 
 
 
-        Bundle extras = getIntent().getExtras();
-        if (extras != null) {
-            final String value = extras.getString("new_variable_name");
-
-
-            APIClient.get(value, null, new JsonHttpResponseHandler() {
-                // GET doesnt need params
-                // BUT IT DOES NEED A PROPER PATH
+            ArrayList<String> urls;
+            urls = getIntent().getStringArrayListExtra("new_variable_name");
 
 
 
-                @Override
-                public void onStart() {
-                        //ans.setText("Processing");
-
-                    listView = (ListView) findViewById(R.id.list);
+            listView = (ListView) findViewById(R.id.list);
 
 
 
 
-                    // Defined Array values to show in ListView
-                    String[] values = new String[] { "Processing", "Hello"
-
-                    };
-
-                    ArrayList<String> planetList = new ArrayList<String>();
-                    planetList.addAll( Arrays.asList(values) );
-                    listAdapter = new ArrayAdapter<String>(hidden.this, R.layout.simplerow, planetList);
-                    listView.setAdapter(listAdapter);
+            // Defined Array values to show in ListView
 
 
 
+            listAdapter = new ArrayAdapter<String>(hidden.this, R.layout.simplerow, urls);
+            listView.setAdapter(listAdapter);
 
 
-
-
-                }
-
-                @Override
-                public void onSuccess(int statusCode, Header[] headers, JSONArray
-                        response) {
-                    // 'JSONArray response' because it will be an array of repos
-
-
-                    for (int i = 0; i < response.length(); i++) {
-                        JSONObject obj = null;
-                        try {
-                            obj = response.getJSONObject(i);
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-                        String name = null;
-                        try {
-                            name = obj.getString("name");
-                        } catch (JSONException e) {
-                            e.printStackTrace();
-                        }
-
-
-                        mylist.add(name); //this adds an element to the list.
-
-
-                    }
-
-
-                    listView = (ListView) findViewById(R.id.list);
-
-
-
-
-                    // Defined Array values to show in ListView
-
-
-
-                    listAdapter = new ArrayAdapter<String>(hidden.this, R.layout.simplerow, mylist);
-                    listView.setAdapter(listAdapter);
-
-
-                }
-
-                @Override
-                //  public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-                public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject error) {
-                    //ans.setText("Error:\n\n" + error);
-
-
-                    listView = (ListView) findViewById(R.id.list);
-
-
-
-
-                    // Defined Array values to show in ListView
-                    String[] values = new String[] { "Error", error.toString()
-
-                    };
-
-                    ArrayList<String> planetList = new ArrayList<String>();
-                    planetList.addAll( Arrays.asList(values) );
-                    listAdapter = new ArrayAdapter<String>(hidden.this, R.layout.simplerow, planetList);
-                    listView.setAdapter(listAdapter);
-
-
-                }
-
-            });
 
 
         }
@@ -155,4 +60,3 @@ static    ArrayList<String> mylist = new ArrayList<String>();
 
 
 
-}
